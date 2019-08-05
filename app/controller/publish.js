cd5a37b59ff627ec6108fbc38952447a1875c128
Dearkano <vaynetian@cc98.org>
2019-08-05 15:46:27 +0800

    update

--- app/controller/publish.js
+++ app/controller/publish.js
@@ -22,19 +22,6 @@ class PublishController extends Controller {
             ctx
         } = this
         let content = null
-        var onData = function (interest, data) {
-            console.log("Got data packet with name " + data.getName().toUri());
-            console.log(data.getContent().buf().toString('binary'));
-            content = data.getContent().buf().toString('binary')
-            console.log('before run')
-            fn.run()
-            ctx.body=content
-            if (++callbackCount >= 3)
-                // This will cause the script to quit.
-                face.close();
-
-        };
-
         var onTimeout = function (interest) {
             console.log("Time out for interest " + interest.getName().toUri());
 
@@ -42,18 +29,25 @@ class PublishController extends Controller {
                 // This will cause the script to quit.
                 face.close();
         };
-        const fn = Fiber(()=>{
-            var name1 = new Name("/bfs/1e000000000304a17ae21be9df24640ebf6ae2a3bd5f1be76c8056cd55beca1f5463ffcac6157e7750ddfc057629f809b61a2f40c357de65311e8ce95d052d19");
-            console.log("Express name " + name1.toUri());
-            face.expressInterest(name1, onData.bind(this), onTimeout);
-            console.log('yield start')
-            Fiber.yield()
-            console.log('yield over')
+        const onData = function (interest, data) {
+            console.log("Got data packet with name " + data.getName().toUri());
+            content = data.getContent().buf().toString('binary')
             ctx.body = content
-            ctx.status = 200
-        })
-        fn.run()
-        while(true){}
+        };
+
+        function asyncInterest() {
+            return new Promise(function (resolve) {
+                const name1 = new Name("/bfs/1e000000000304a17ae21be9df24640ebf6ae2a3bd5f1be76c8056cd55beca1f5463ffcac6157e7750ddfc057629f809b61a2f40c357de65311e8ce95d052d19");
+                console.log("Express name " + name1.toUri());
+                face.expressInterest(name1, (_, data) => resolve(data), onTimeout);
+            })
+        }
+        const data = await asyncInterest()
+        content = data.getContent().buf().toString('binary')
+        ctx.body = content
+        ctx.status = 200
+
+        //while(true){}
     }
 }
 module.exports = PublishController;
