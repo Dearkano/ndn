ee93bcdb8dd1da983881984da82ebf8d114c8e93
Dearkano <vaynetian@cc98.org>
2019-08-05 15:07:32 +0800

    update

--- app/controller/publish.js
+++ app/controller/publish.js
@@ -17,30 +17,15 @@ var callbackCount = 0;
 
 
 class PublishController extends Controller {
-    constructor(ctx) {
-        super(ctx)
-        const fiber = Fiber.current
-        console.log('in constructor')
-        console.log(fiber)
-        this.fiber = fiber
-    }
     async getFileInfo() {
         const {
             ctx
         } = this
-        var name1 = new Name("/bfs/1e000000000304a17ae21be9df24640ebf6ae2a3bd5f1be76c8056cd55beca1f5463ffcac6157e7750ddfc057629f809b61a2f40c357de65311e8ce95d052d19");
-        console.log("Express name " + name1.toUri());
-        let content = null
         var onData = function (interest, data) {
             console.log("Got data packet with name " + data.getName().toUri());
             console.log(data.getContent().buf().toString('binary'));
             content = data.getContent().buf().toString('binary')
-            console.log(this)
-            console.log(this.fiber)
-            Fiber(() => {
-                console.log('fiber run')
-                ctx.body = '123'
-            }).run()
+            Fiber.current.run()
             if (++callbackCount >= 3)
                 // This will cause the script to quit.
                 face.close();
@@ -53,10 +38,16 @@ class PublishController extends Controller {
                 // This will cause the script to quit.
                 face.close();
         };
-        face.expressInterest(name1, onData.bind(this), onTimeout);
-        Fiber.yield()
-        ctx.body = content
-        ctx.status = 200
+        const fn = Fiber(()=>{
+            var name1 = new Name("/bfs/1e000000000304a17ae21be9df24640ebf6ae2a3bd5f1be76c8056cd55beca1f5463ffcac6157e7750ddfc057629f809b61a2f40c357de65311e8ce95d052d19");
+            console.log("Express name " + name1.toUri());
+            let content = null
+    
+            face.expressInterest(name1, onData.bind(this), onTimeout);
+            Fiber.yield()
+            ctx.body = content
+            ctx.status = 200
+        }).run()
     }
 }
 module.exports = PublishController;
