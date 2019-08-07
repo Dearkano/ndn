47816e4da5848e65e2ea2a4db728b34550606012
Dearkano <vaynetian@cc98.org>
2019-08-07 17:13:32 +0800

    update

--- app/service/afs.js
+++ app/service/afs.js
@@ -3,6 +3,7 @@ const Service = require('egg').Service;
 
 class AfsService extends Service {
   async getRNodeInfo(ip) {
+      console.log(ip)
     const res = await fetch(ip)
     const text = await res.text()
     return text;
