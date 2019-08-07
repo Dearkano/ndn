247f3ddbdef131e962ef8df70b0135f170825ebe
Dearkano <vaynetian@cc98.org>
2019-08-07 17:20:15 +0800

    update

--- app/controller/afs.js
+++ app/controller/afs.js
@@ -5,8 +5,7 @@ class AfsController extends Controller {
         const {
             ctx
         } = this
-        console.log(ctx.request.body)
-        const ip = JSON.parse(ctx.request.body).ip
+        const ip = ctx.request.body.ip
         const data = await this.service.afs.getRNodeInfo(ip)
         ctx.body = data
         ctx.status = 200
