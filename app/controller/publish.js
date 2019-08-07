b8cc95e0a57eda45bb5c62362a767ee7b25f6c65
Dearkano <vaynetian@cc98.org>
2019-08-07 10:56:14 +0800

    update

--- app/controller/publish.js
+++ app/controller/publish.js
@@ -101,6 +101,7 @@ class PublishController extends Controller {
         // send request to request basic information
         const name = new Name(`/bfs/pre/afid/${afid}`);
         const res = await asyncInterest(name)
+        console.log(res)
         const resStr = res.data.getContent().buf().toString()
         console.log(resStr)
         const blockNum = JSON.parse(resStr).blockNum
