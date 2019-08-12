3c10664ccfc6c9fbf5f8f9f4f5ec61a06a270e3e
Dearkano <vaynetian@cc98.org>
2019-08-12 15:19:57 +0800

    update

--- app/router.js
+++ app/router.js
@@ -4,7 +4,7 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  const { router, controller } = app;
+  const { router, controller } = app.io;
   router.get('/', controller.home.index);
   router.get('/getFileInfo', controller.publish.getFileInfo);
   router.get('/download', controller.publish.downloadFile);
