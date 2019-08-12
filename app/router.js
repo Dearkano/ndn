046326d454f9001a9746729deb819c1e833f61c7
Dearkano <vaynetian@cc98.org>
2019-08-12 15:53:55 +0800

    update

--- app/router.js
+++ app/router.js
@@ -6,15 +6,17 @@
 module.exports = app => {
     const {
         route,
-        router,
         controller
     } = app.io;
-      router.get('/', controller.home.index);
-      router.get('/getFileInfo', controller.publish.getFileInfo);
-      router.get('/download', controller.publish.downloadFile);
-      router.get('/downloadFile', controller.publish.downloadIFile);
-      router.get('/getDownloadInfo', controller.publish.getDownloadInfo);
-      router.post('/getRNodeInfo', controller.afs.getRNodeInfo)
-      router.post('/file_record/afid', controller.publish.getFileRecord)
+    const {
+        router
+    } = app
+    router.get('/', controller.home.index);
+    router.get('/getFileInfo', controller.publish.getFileInfo);
+    router.get('/download', controller.publish.downloadFile);
+    router.get('/downloadFile', controller.publish.downloadIFile);
+    router.get('/getDownloadInfo', controller.publish.getDownloadInfo);
+    router.post('/getRNodeInfo', controller.afs.getRNodeInfo)
+    router.post('/file_record/afid', controller.publish.getFileRecord)
     app.io.route('chat', app.io.controller.chat.index);
 };
