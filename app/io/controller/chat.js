e5d197747c20236f60b88eccedcad208b6ab102b
Dearkano <vaynetian@cc98.org>
2019-08-19 17:13:55 +0800

    update

--- app/io/controller/chat.js
+++ app/io/controller/chat.js
@@ -1,48 +1,48 @@
-// const Controller = require('egg').Controller;
-// const {
-//     Face,
-//     Name,
-//     UnixTransport,
-//     Interest
-// } = require('ndn-js-sdk')
-// Interest.setDefaultCanBePrefix(true);
-// const face = new Face(new UnixTransport());
+const Controller = require('egg').Controller;
+const {
+    Face,
+    Name,
+    UnixTransport,
+    Interest
+} = require('ndn-js-sdk')
+Interest.setDefaultCanBePrefix(true);
+const face = new Face(new UnixTransport());
 
-// class ChatController extends Controller {
-//     // async send() {
-//     //     const {
-//     //         ctx
-//     //     } = this
-//     //     const {
-//     //         receiver,
-//     //         data
-//     //     } = ctx.request.body
-//     //     await this.service.chat.send(receiver, data)
-//     //     ctx.status = 200
-//     // }
-
-//     async test() {
-//         const message = this.ctx.args[0];
-//         console.log('chat :', message + ' : ' + process.pid);
-//         // const say = await this.ctx.service.user.say();
-//         this.ctx.socket.emit('res', say);
-//     }
-
-//     async receive() {
-
-//     }
-// }
-
-// module.exports = ChatController
-'use strict';
-
-module.exports = app => {
-  class Controller extends app.Controller {
-    async index() {
-      const message = this.ctx.args[0];
-      console.log('chat :', message + ' : ' + process.pid);
-      this.ctx.socket.emit('res', message);
+class ChatController extends Controller {
+    async send() {
+        const {
+            ctx
+        } = this
+        const {
+            receiver,
+            data
+        } = ctx.request.body
+        await this.service.chat.send(receiver, data)
+        ctx.status = 200
     }
-  }
-  return Controller;
-};
+
+    async index() {
+        const message = this.ctx.args[0];
+        console.log('chat :', message + ' : ' + process.pid);
+        // const say = await this.ctx.service.user.say();
+        this.ctx.socket.emit('res', message);
+    }
+
+    async receive() {
+
+    }
+}
+
+module.exports = ChatController
+// 'use strict';
+
+// module.exports = app => {
+//   class Controller extends app.Controller {
+//     async index() {
+//       const message = this.ctx.args[0];
+//       console.log('chat :', message + ' : ' + process.pid);
+//       this.ctx.socket.emit('res', message);
+//     }
+//   }
+//   return Controller;
+// };
