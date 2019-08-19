304bdfe312e468c4e6d11b900561e73bcdb6bba2
Dearkano <vaynetian@cc98.org>
2019-08-19 17:08:40 +0800

    update

--- app/io/controller/chat.js
+++ app/io/controller/chat.js
@@ -9,21 +9,24 @@ Interest.setDefaultCanBePrefix(true);
 const face = new Face(new UnixTransport());
 
 class ChatController extends Controller {
-    async send() {
-        const {
-            ctx
-        } = this
-        const {receiver, data} = ctx.request.body
-        await this.service.chat.send(receiver, data)
-        ctx.status = 200
-    }
+    // async send() {
+    //     const {
+    //         ctx
+    //     } = this
+    //     const {
+    //         receiver,
+    //         data
+    //     } = ctx.request.body
+    //     await this.service.chat.send(receiver, data)
+    //     ctx.status = 200
+    // }
 
     async test() {
         const message = this.ctx.args[0];
         console.log('chat :', message + ' : ' + process.pid);
         // const say = await this.ctx.service.user.say();
         this.ctx.socket.emit('res', say);
-      }
+    }
 
     async receive() {
 
