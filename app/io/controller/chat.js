38470712a9906621aba4bc8c5a8e1bf28ae1e661
Dearkano <vaynetian@cc98.org>
2019-08-19 17:11:06 +0800

    update

--- app/io/controller/chat.js
+++ app/io/controller/chat.js
@@ -41,8 +41,7 @@ module.exports = app => {
     async index() {
       const message = this.ctx.args[0];
       console.log('chat :', message + ' : ' + process.pid);
-      const say = await this.ctx.service.user.say();
-      this.ctx.socket.emit('res', say);
+      this.ctx.socket.emit('res', message);
     }
   }
   return Controller;
