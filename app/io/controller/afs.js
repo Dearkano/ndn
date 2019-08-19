58526a34254e62838f164a00c9e99b2a16443776
Dearkano <vaynetian@cc98.org>
2019-08-19 17:12:29 +0800

    update

--- app/io/controller/afs.js
+++ app/io/controller/afs.js
@@ -1,15 +1,15 @@
-// const Controller = require('egg').Controller;
+const Controller = require('egg').Controller;
 
-// class AfsController extends Controller {
-//     async getRNodeInfo() {
-//         const {
-//             ctx
-//         } = this
-//         const ip = ctx.request.body.ip
-//         const data = await this.service.afs.getRNodeInfo(ip)
-//         ctx.body = data
-//         ctx.status = 200
-//     }
-// }
+class AfsController extends Controller {
+    async getRNodeInfo() {
+        const {
+            ctx
+        } = this
+        const ip = ctx.request.body.ip
+        const data = await this.service.afs.getRNodeInfo(ip)
+        ctx.body = data
+        ctx.status = 200
+    }
+}
 
-// module.exports = AfsController
+module.exports = AfsController
--- app/router.js
+++ app/router.js
@@ -11,13 +11,13 @@ module.exports = app => {
     const {
         router
     } = app
-    // router.get('/', controller.home.index);
-    // router.get('/getFileInfo', controller.publish.getFileInfo);
-    // router.get('/download', controller.publish.downloadFile);
-    // router.get('/downloadFile', controller.publish.downloadIFile);
-    // router.get('/getDownloadInfo', controller.publish.getDownloadInfo);
-    // router.post('/getRNodeInfo', controller.afs.getRNodeInfo)
-    // router.post('/file_record/afid', controller.publish.getFileRecord)
-    // router.get('/getFileParameter', controller.publish.getFileParameter);
+    router.get('/', controller.home.index);
+    router.get('/getFileInfo', controller.publish.getFileInfo);
+    router.get('/download', controller.publish.downloadFile);
+    router.get('/downloadFile', controller.publish.downloadIFile);
+    router.get('/getDownloadInfo', controller.publish.getDownloadInfo);
+    router.post('/getRNodeInfo', controller.afs.getRNodeInfo)
+    router.post('/file_record/afid', controller.publish.getFileRecord)
+    router.get('/getFileParameter', controller.publish.getFileParameter);
     app.io.route('chat', app.io.controller.chat.index);
 };
