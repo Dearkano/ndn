e3c92b66078c49717d831f7517d47984b5c85b9e
Dearkano <vaynetian@cc98.org>
2019-08-20 15:21:35 +0800

    update

--- ndn.js
+++ ndn.js
@@ -166,8 +166,8 @@ Echo.prototype.onInterest = async function (prefix, interest, face, interestFilt
     //const ctx = this.app.createAnonymousContext();
     // ctx.service.chat.reply('this is my reply -')
     //this.app.io.controller.chat.reply('this is my reply =')
-    console.log(this.app.io)
-    this.app.io.emit('res', 'this is broadcast')
+    console.log(this.app.io.sockets)
+    this.app.io.sockets.emit('res', 'this is broadcast')
     const data1 = new Data(interest.getName());
     data1.setContent(data);
     that.keyChain.sign(data1);
