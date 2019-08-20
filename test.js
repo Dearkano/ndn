cfc7b087e375a2e6fca7b3047cede73cb7ceb30d
Dearkano <vaynetian@cc98.org>
2019-08-20 14:02:24 +0800

    update

---
+++ test.js
@@ -0,0 +1,8 @@
+var io = require('socket.io-emitter')({
+    host: '127.0.0.1',
+    port: 6379
+});
+setInterval(function () {
+    io.emit('time', new Date);
+}, 5000);
+io.emit('broadcast', 'cccc');
