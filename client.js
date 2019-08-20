42a6747a1e753929c87ec969d76717e45d511fc2
Dearkano <vaynetian@cc98.org>
2019-08-20 15:30:54 +0800

    update

--- client.js
+++ client.js
@@ -4,15 +4,16 @@
 const socket = require('socket.io-client')(`http://${process.argv[2]}`);
 
 socket.on('connect', () => {
-  console.log('connect!');
-  const data = {
-      sender: process.argv[3],
-      receiver: process.argv[4],
-      data: `Hello, ${process.argv[4]}, I am ${process.argv[3]}`
-  }
-  socket.emit('chat', JSON.stringify(data));
+    console.log('connect!');
+    const data = {
+        sender: process.argv[3],
+        receiver: process.argv[4],
+        data: `Hello, ${process.argv[4]}, I am ${process.argv[3]}`
+    }
+    console.log('sending: ' + JSON.stringify(data));
+    socket.emit('chat', JSON.stringify(data));
 });
 
 socket.on('res', msg => {
-  console.log('res from server: %s!', msg);
+    console.log('res from server: %s!', msg);
 });
